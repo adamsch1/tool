28f8fbb58a47b2202fb02f3fd19b407c0337d6db
shane adasm <adamsch1@yahoo.com>
2016-06-14 12:42:43 -0700

    comments

--- con.cc
+++ con.cc
@@ -7,8 +7,6 @@
 #include <sys/sysinfo.h>
 
 #define MEGA (1024*1024llu)
-#define MSIZE (MEGA*1024*2)
-#define MOFFSET (MSIZE/sizeof(uint64_t))
 
 #include <parallel/algorithm>
 #include <algorithm>
@@ -25,11 +23,11 @@ int main( int argc, char **argv ) {
 	uint64_t *buffer;
 	uint64_t data;
 	uint64_t k=0;
-	uint64_t bcount=0;
 	uint64_t mpercent = 50;
 	uint64_t msize = 0;
 	uint64_t moffset = 0;
 
+	// Only argument is %X where X is 1-100 specifing how much system memory to use
 	if( argc > 1 ) {
 		if( argv[1][0] == '%' )  {
 			uint64_t size = atoi( &argv[1][1] );
@@ -37,29 +35,30 @@ int main( int argc, char **argv ) {
 		}
 	}
 
+	// Calculate how much memory we have and the nallocate mpercent of that for our use
 	struct sysinfo info = {0};
 	sysinfo( &info );
 	msize = (uint64_t)(info.totalram * mpercent / 100.0);
 	while( (buffer= (uint64_t*)malloc( msize )) == NULL ) {
+		// In case we cannot allocate fallback half
 		msize /= 2;
 	}
 
+	// Max number of entries given size of our buffer
 	moffset = msize / sizeof(uint64_t);
 
 	while( fread( &data, sizeof(data),1, stdin) > 0)  {
 		buffer[k++] = data;
 		if( k == moffset ) {
-			bcount++;
 #ifdef PMODE
-//			__gnu_parallel::sort( buffer, buffer + k );
+			// I found std::sort to be fastest on my system, hence g++
 			std::sort( buffer, buffer + k );
 #else 
 			qsort( buffer, moffset, sizeof(uint64_t), icmp );
 #endif
-			fwrite( buffer, MSIZE, 1, stdout );
+			fwrite( buffer, sizeof(uint64_t), k, stdout );
 			k = 0;
 		}
 	}
-	printf("%ld %llu %llu\n",bcount, MSIZE, MOFFSET);
 }
 
