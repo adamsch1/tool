2b6220a91c40d02dd37be83456669664e8b4fe61
shane adasm <adamsch1@yahoo.com>
2016-06-15 09:09:58 -0700

    output file directly

--- con.cc
+++ con.cc
@@ -8,6 +8,25 @@
 #include <algorithm>
 #include <iterator>
 
+int dumper( char *tmplate, char *buffer, uint64_t blength ) {
+	FILE *outs;
+	int fd;
+	char *dupe = strdup(tmplate);
+
+	fd = mkstemp( dupe );
+	if( fd == -1 ) {
+		//error("Could not open output file");
+	}
+
+	outs = fdopen( fd, "r+b");
+	fwrite( buffer, blength, 1, outs );
+
+	fclose( outs );
+	free( dupe );
+
+	return 0;
+}
+
 int main( int argc, char **argv ) {
 
 	uint64_t *buffer;
@@ -16,12 +35,18 @@ int main( int argc, char **argv ) {
 	uint64_t mpercent = 50;
 	uint64_t msize = 0;
 	uint64_t moffset = 0;
+	char * tmplate = strdup("con_XXXXXX");
 
 	// Only argument is %X where X is 1-100 specifing how much system memory to use
 	if( argc > 1 ) {
 		if( argv[1][0] == '%' )  {
 			uint64_t size = atoi( &argv[1][1] );
 			if( size > 0 && size < 100 ) mpercent = size;  
+		}
+
+		if( argc > 2 ) {
+			// I lied, also output template
+			tmplate = argv[2];
 		}
 	}
 
@@ -43,9 +68,11 @@ int main( int argc, char **argv ) {
 		if( k == moffset ) {
 			// I found std::sort to be fastest on my system, hence g++
 			std::sort( buffer, buffer + k );
-			fwrite( buffer, sizeof(uint64_t), k, stdout );
+			dumper( tmplate, (char*)buffer, k * sizeof(uint64_t));
 			k = 0;
 		}
 	}
+	dumper( tmplate, (char*)buffer, k * sizeof(uint64_t));
+	free(tmplate);
 }
 
